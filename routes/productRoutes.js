b7a29042e18d9ef5b10d40079ef48aae2a334c17
Julian Lescano <lescano.julian22@gmail.com>
2023-01-10 11:11:15 -0300

    get product by id endpoint fixed

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -62,8 +62,8 @@ router.delete('/:id', async (req,res) => {
 router.get('/:id', async (req, res) => {
     const {id} = req.params;
     try {
-        const product = Product.findById(id);
-        const similar = (await Product.find({category: product.category})).limit(5);
+        const product =  await Product.findById(id);
+        const similar = await Product.find({category: product.category}).limit(5);
         res.status(200).json({product, similar});
     } catch (e) {
         res.status(400).send(e.message);
